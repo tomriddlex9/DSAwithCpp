6bb1e8f940cbd41138d5700e668ac4b8061a4a14
tomriddlex9 <siddhantsiwach1999@gmail.com>
2021-10-25 16:19:16 +0530

    minor changes

--- LinkedLists/basicLL.cpp
+++ LinkedLists/basicLL.cpp
@@ -1,39 +1,72 @@
-#include<iostream>
+#include<iostream> 
 using namespace std; 
-struct Node{
-    int value; 
-    struct Node *link; 
+class Node
+{
+    public: 
+    int data; 
+    Node *next; 
+    Node(int new_data)
+    {
+        data=new_data; 
+        next=NULL; 
+    }
 };
-void display(Node *start)
+void pushToFront(Node **head_ref, int new_data )
 {
-    //create a pointer to the struct node
-    struct Node *p; 
-    // check if the linked list is not empty
-    if(start==NULL)
-    {
-        cout<<"List is Empty\n";
-        return ; 
-    }
-    // assign the pointer to starting node 
-    p=start; 
-    cout<<"List is: \n";
-    // iterate untill the end of the linked list is reached 
-    while(p!=NULL)
-    {
-        cout<<p->value<<"\n";
-        p=( p->link );  
-    }
+    Node *newnode= new Node(new_data);
+    newnode->next=(*head_ref);
+    (*head_ref)=newnode;    
 }
-void insert(Node)
+void insertAfterNode(Node *prev_node,int new_data)
 {
-    
+    if(prev_node==NULL) 
+    {
+        cout<<"Previous Node cannot be NULL";
+        return; 
+    }
+    Node *newnode=new Node(new_data);
+    newnode->next=prev_node->next; 
+    prev_node->next=newnode; 
+}
+void insertAtEnd(Node ** head_ref,int new_data)
+{
+    Node *newnode=new Node(new_data);
+    Node *last=*head_ref; 
+    newnode->next=NULL; 
+    if(*head_ref=NULL)
+    {
+        *head_ref=newnode; 
+        return ;
+    }
+    while(last->next!=NULL )
+    {
+        last=last->next; 
+    }
+    last->next=newnode; 
+    return; 
+
+
+}
+void printLinkedList(Node *n)
+{
+    while(n!=NULL)
+    {
+        cout<<(n->data)<<" ";
+        n=n->next; 
+    }
+
 }
 
-int main(int argc, const char** argv) 
+int main()
 {
-    Node head; 
-    Node *p ; 
-       
-    
-    return 0;
+    Node *head=new Node(2);
+    pushToFront(&head,1);
+    printLinkedList(head);
+    pushToFront(&head,0);
+    insertAtEnd(&head,4); 
+    insertAfterNode(head->next->next->next,3);
+    printLinkedList(head);
+    return 0; 
+
+
 }

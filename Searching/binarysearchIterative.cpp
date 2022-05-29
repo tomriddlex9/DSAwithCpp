160d5afedbaf7f44cbfb6778e0193aaceb275ec4
tomriddlex9 <siddhantsiwach1999@gmail.com>
2022-05-29 06:20:02 +0530

    Searching

--- Searching/binarysearchIterative.cpp
+++ Searching/binarysearchIterative.cpp
@@ -1,28 +1,34 @@
-#include<iostream> 
-using namespace std; 
-int binarySearch(int array[],int n,int key){
-    int low=0,high=n-1; 
-    while(low<=high)
+#include <iostream>
+using namespace std;
+int binarySearch(int array[], int n, int key)
+{
+    int low = 0, high = n - 1;
+    // since we are constantly incrementing the low and decrementing the low
+    // this is the check to see if the search is still in the bounds of the array
+    while (low <= high)
     {
-        int mid=(low+high)/2;
-        if(array[mid]==key) 
+        // get the middle element
+        int mid = (low + high) / 2;
+        int midBetter = low + (high - low) / 2;
+        if (array[mid] == key)
         {
-            return mid; // exit point 
+            return mid; // exit point
         }
-        else if(key<array[mid])
+        else if (key < array[mid])
         {
-            high=mid-1; 
-        } 
-        else 
-        {
-            low=mid+1; 
+            high = mid - 1;
         }
-    } 
-    return -1;  // if key not found then this is exit point
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return -1; // if key not found then this is exit point
 }
-int main(){ 
-    int array[10]={2,6,8,9,56,67,87,88,92,99};
-    int key; 
-    cin>>key; 
-    cout<<binarySearch(array,10,key)<<endl;    
+int main()
+{
+    int array[10] = {2, 6, 8, 9, 56, 67, 87, 88, 92, 99};
+    int key;
+    cin >> key;
+    cout << binarySearch(array, 10, key) << endl;
 }
